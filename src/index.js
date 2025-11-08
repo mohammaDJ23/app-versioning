0091778629588506e48f6ae79c598eb9041d2d88
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2025-11-08 16:24:02 +0330

    Revert the changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-const { User, Users } = require('./user');
-
 function main() {
-  const users = new Users();
-
-  const user = new User('Mohammad', 'Nowresideh', 28);
-  
-  user.log();
-
-  users.push(user);
+  console.log('hello');
 }
 
 main();
