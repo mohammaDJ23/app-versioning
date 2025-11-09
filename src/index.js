4ce2a7a678d7cbba85f466be22ccc02573e72ee5
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2025-11-09 15:20:37 +0330

    refactor(index): Remove the user calling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 const { Auth } = require('./auth');
-const { User } = require('./user');
 
 function main() {
   const auth = new Auth();
-  const user = new User('Mohammad', 'Nowresideh');
 
   auth.login();
-
-  user.log();
-  user.getFullName();
 }
 
 main();
