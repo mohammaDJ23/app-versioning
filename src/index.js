821b491852cd1037c51bec2c6482763006309118
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2025-11-10 09:40:22 +0330

    chore(index): Use dashboard

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const { User } = require('./user');
+const { Dashboard } = require('./dashboard');
 
 function main() {
   const user = new User('Mohammad', 'Nowresideh');
-  
+  const dashboard = new Dashboard(user);
+
   console.log(user);
+  console.log(dashboard);
   console.log('hello world');
 }
 
