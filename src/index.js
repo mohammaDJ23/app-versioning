c7afe541d931e43ad079e409bc07637fbeaa7968
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2025-11-12 12:49:58 +0330

    chore(index): Use the dashboard

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+const { Dashboard } = require('./dashboard');
 const { User } = require('./user');
 
 function main() {
   const user = new User('Mohammad', 'Nowresideh');
+  const dashboard = new Dashboard(user);
 
   console.log('A new user created which is called', user);
+  console.log('A new dashboard created which is called', dashboard);
   console.log('hello world.');
 }
 
